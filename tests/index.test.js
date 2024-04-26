8a271253bd65f7572fb385b35fc098241d6872d7
ChrisCodesThings <153457755+ChrisCodesThings@users.noreply.github.com>
2024-04-26 17:39:22 +0100

    tests: remove unnessary tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,24 +1,6 @@
 
 const { default: testFunc } = await import("../");
 
-describe("check for bad input", () => {
-    test("nothing", async () => {
-        expect(testFunc()).toBeUndefined();
-    });
-
-    test("boolean", async () => {
-        expect(testFunc(true)).toBeUndefined();
-    });
-
-    test("a number", async () => {
-        expect(testFunc(123)).toBeUndefined();
-    });
-
-    test("a string", async () => {
-        expect(testFunc("foo")).toBeUndefined();
-    });
-});
-
 describe("convert some rgba colours to hex", () => {
     test("black", () => {
         expect(testFunc([0, 0, 0, 1])).toEqual("#000"); // black
