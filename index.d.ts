4843950579aa88e348d5e3aaf96c68b3d9f08d39
ChrisCodesThings <153457755+ChrisCodesThings@users.noreply.github.com>
2024-05-12 20:51:45 +0100

    fix: move types to external package

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,1 @@
 export { default } from "./src/rgbaColorToCSSHex.js";
-export * from "./types.js";
--- src/rgbaColorToCSSHex.d.ts
+++ src/rgbaColorToCSSHex.d.ts
@@ -1,7 +1,7 @@
 /**
  * Converts a colour from RGBA format to a CSS hex colour code
- * @param {Types.rgbColor | Types.rgbaColor} rgba Array containing red, green, blue and (optional) alpha values
- * @returns {string}
+ * @param {colorTypes.rgbColor | colorTypes.rgbaColor} rgba Array containing red, green, blue and (optional) alpha values
+ * @returns {colorTypes.cssHexCode}
  */
-export default function rgbaColorToCSSHex(rgba: Types.rgbColor | Types.rgbaColor): string;
-import * as Types from '../types';
+export default function rgbaColorToCSSHex(rgba: colorTypes.rgbColor | colorTypes.rgbaColor): colorTypes.cssHexCode;
+import * as colorTypes from '@chriscodesthings/basic-color-types';
